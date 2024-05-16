845789a544c54250df341614e591bb6c06e0412d
ahmedhassayoune <ahmedhassayoune@users.noreply.github.com>
2024-05-16 12:30:19 +0000

    [RUFF FORMAT]

--- scripts/tri_to_quad.py
+++ scripts/tri_to_quad.py
@@ -1,17 +1,18 @@
 import bpy
 import bmesh
 
+
 def prepareTriangle(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     """
     Transform the mesh to a redefinition of it with an even number of
     triangles.
-    
+
     This function search a border edge and split it in half.
     (Condition : Not closed)
 
     If the mesh is closed (= no border edge) and two-manifold, there is
     always an even number of triangles.
-    
+
     Parameters:
     - mesh (bmesh.types.BMesh): The input triangle BMesh object representing
                                 the mesh to be converted.
@@ -22,21 +23,25 @@ def prepareTriangle(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
 
     Note:
     This function modifies the input mesh in-place and returns the same object.
-    """  
+    """
     mesh.edges.ensure_lookup_table()
-    
+
     # Search of a border edge
     borderEdge = None
     for edge in mesh.edges:
         if edge.is_boundary:
             borderEdge = edge
             break
-    
-    if borderEdge is None: # If this condition is reached, the mesh is invalid (not two-manifold)
-        raise RuntimeError('Call to prepareTriangle with a closed mesh (not two-manifold mesh ?).') 
-    
+
+    if (
+        borderEdge is None
+    ):  # If this condition is reached, the mesh is invalid (not two-manifold)
+        raise RuntimeError(
+            "Call to prepareTriangle with a closed mesh (not two-manifold mesh ?)."
+        )
+
     faceToDivide = borderEdge.link_faces[0]
-    
+
     # Search of the opposed vertex
     opposedVert = None
     for vert in faceToDivide.verts:
@@ -48,13 +53,15 @@ def prepareTriangle(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     middle = mesh.verts.new((borderEdge.verts[0].co + borderEdge.verts[1].co) / 2)
     mesh.faces.new([middle, borderEdge.verts[0], opposedVert])
     mesh.faces.new([middle, borderEdge.verts[1], opposedVert])
-    
+
     mesh.edges.remove(borderEdge)
-    
+
     return mesh
 
+
 def merge(
-    t0: bmesh.types.BMFace, t1: bmesh.types.BMFace,
+    t0: bmesh.types.BMFace,
+    t1: bmesh.types.BMFace,
 ) -> (bmesh.types.BMEdge, [bmesh.types.BMVert]):
     """
     Returns the edge to remove and the vertices of the theoric face that
@@ -64,7 +71,7 @@ def merge(
         if edge in t1.edges:
             commonEdge = edge
             break
-    
+
     for vert in t0.verts:
         if vert not in commonEdge.verts:
             vert_t0 = vert
@@ -80,33 +87,34 @@ def merge(
 
     return commonEdge, verts
 
+
 def squareness(edge: bmesh.types.BMEdge) -> float:
     """
     Calculate sum of pairwise dot products of the four normalized edges
     of the theoric face that would result if the parameter edge was dissolved.
     """
     if edge.is_boundary:
-        return float('inf') # Bigger than the maximum possible squareness
-    
+        return float("inf")  # Bigger than the maximum possible squareness
+
     # Get the 4 vertices of the theoric face in the right order
     _, vertices = merge(edge.link_faces[0], edge.link_faces[1])
-    
+
     # Compute the normalized edges
     v01 = (vertices[1].co - vertices[0].co).normalized()
     v03 = (vertices[3].co - vertices[0].co).normalized()
     v21 = (vertices[1].co - vertices[2].co).normalized()
     v23 = (vertices[3].co - vertices[2].co).normalized()
-    
+
     # Compute the sum of pairwise dot products
-    dot_sum = 0 
+    dot_sum = 0
     dot_sum += abs(v01.dot(v03))
     dot_sum += abs(v21.dot(v23))
 
     return dot_sum
 
+
 def neighborsOf(
-    triangle0: bmesh.types.BMFace,
-    triangle1: bmesh.types.BMFace
+    triangle0: bmesh.types.BMFace, triangle1: bmesh.types.BMFace
 ) -> [bmesh.types.BMFace]:
     """
     Returns the list of neighbors faces from the theoric face that would
@@ -117,17 +125,17 @@ def neighborsOf(
 
     for triangle in triangles:
         for edge in triangle.edges:
-            if not edge.is_boundary: # Else, the only face linked to it is triangle
+            if not edge.is_boundary:  # Else, the only face linked to it is triangle
                 for i in range(2):
                     face = edge.link_faces[i]
                     if face not in triangles and face not in neighbors:
                         neighbors.append(face)
-    
+
     return neighbors
 
+
 def commonEdgeOf(
-    face1: [bmesh.types.BMFace],
-    face2: [bmesh.types.BMFace]
+    face1: [bmesh.types.BMFace], face2: [bmesh.types.BMFace]
 ) -> (bmesh.types.BMEdge, bmesh.types.BMFace, bmesh.types.BMFace):
     """
     Returns a common edge between the two theoric faces, each composed of triangles, and the
@@ -139,18 +147,16 @@ def commonEdgeOf(
                 if edge in subface2.edges:
                     return edge, subface1, subface2
 
-    raise RuntimeError('No common edge between the two theoric faces.')
+    raise RuntimeError("No common edge between the two theoric faces.")
+
 
 def getShortestPathToTriangleFrom(
-    mesh: bmesh.types.BMesh,
-    triangle: bmesh.types.BMFace,
-    mergeList,
-    squarenessList
+    mesh: bmesh.types.BMesh, triangle: bmesh.types.BMFace, mergeList, squarenessList
 ) -> [bmesh.types.BMFace]:
     """
     Find the shortest* path between the unlinked parameter triangle and the
     first other unlinked triangle found by a breadth first visit.
-    
+
     * shortest : the path with the smaller number of faces visited. Ties are
                  broken in favor of the minimal squareness score.
     Returns:
@@ -179,16 +185,16 @@ def getShortestPathToTriangleFrom(
 
         # Neighbors of the theoric rectangle
         neighbors = neighborsOf(current, paths[current][-1])
-        
+
         # Visit on all neighbors
         for neighbor in neighbors:
             if paths[neighbor] is not None:
-                # The actual path to neighbor is shorter 
+                # The actual path to neighbor is shorter
                 if len(paths[neighbor]) < len(paths[current]) + 1:
                     continue
 
                 # Here, the actual path to neighbor have the same number of faces
-                # ----> Squareness tie break <----   
+                # ----> Squareness tie break <----
 
                 # The neighbor is an unlinked triangle -> Squareness of the common edge
                 # between the triangle and the previous rectangle
@@ -204,12 +210,12 @@ def getShortestPathToTriangleFrom(
                     actualOrigin = [paths[neighbor][-3], paths[neighbor][-4]]
                     newOrigin = [paths[current][-1], paths[current][-2]]
 
-                actualEdge,_,_ = commonEdgeOf(actualOrigin, destination)
-                newEdge,_,_ = commonEdgeOf(newOrigin, destination)
+                actualEdge, _, _ = commonEdgeOf(actualOrigin, destination)
+                newEdge, _, _ = commonEdgeOf(newOrigin, destination)
 
                 actualSquareness = squarenessList[actualEdge]
                 newSquareness = squarenessList[newEdge]
-                
+
                 # The actual path to neighbor is shorter
                 if actualSquareness <= newSquareness:
                     continue
@@ -219,7 +225,7 @@ def getShortestPathToTriangleFrom(
                 queue.append(neighbor)
 
             paths[neighbor] = paths[current] + [neighbor]
-            
+
             # The linked face must be taken in the path
             neighborLink = mergeList[neighbor]
             if neighborLink is not None:
@@ -227,13 +233,16 @@ def getShortestPathToTriangleFrom(
                 paths[neighborLink] = paths[neighbor]
 
     print(paths)
-    raise RuntimeError('Breadth first visit failed to find another reachable triangle (not connected mesh ?).')
+    raise RuntimeError(
+        "Breadth first visit failed to find another reachable triangle (not connected mesh ?)."
+    )
+
 
 def correctQuad(quad: bmesh.types.BMFace) -> bmesh.types.BMFace:
     """
     Transform a quad mesh to put all its vertices on the same plane, while
     attempting to preserve the structure.
-    
+
     Note:
     This function modifies the input mesh in-place and returns the same object.
     """
@@ -249,8 +258,9 @@ def correctQuad(quad: bmesh.types.BMFace) -> bmesh.types.BMFace:
     v2.co += center - middle_O2
     v1.co += center - middle_13
     v3.co += center - middle_13
-    
+
     return quad
+
 
 def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     """
@@ -281,7 +291,7 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     if len(mesh.faces) % 2 == 1:
         # Transform the mesh to a redefinition of it with an even number of triangles.
         prepareTriangle(mesh)
-    
+
     # Tool used to preserve the overall shape and structure
     mesh.edges.ensure_lookup_table()
     squarenessList = {edge: squareness(edge) for edge in mesh.edges}
@@ -289,39 +299,45 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     # Tool used to realise the quad mesh conversion
     mesh.faces.ensure_lookup_table()
     mergeList = {face: None for face in mesh.faces}
-    
+
     # -- Quad-dominant mesh: theoric edge collapse --
-    
+
     # Tool used to know best candidate edge to be dissolved for each triangle
-    optimalEdges = {face: min(face.edges, key=lambda x: squarenessList[x]) for face in mesh.faces}
-    
+    optimalEdges = {
+        face: min(face.edges, key=lambda x: squarenessList[x]) for face in mesh.faces
+    }
+
     for face in mesh.faces:
-        if mergeList[face] is not None: # Current face is already linked
+        if mergeList[face] is not None:  # Current face is already linked
             continue
 
         optimalEdge = optimalEdges[face]
-        
-        if optimalEdge.is_boundary: # Current face is isolated
+
+        if optimalEdge.is_boundary:  # Current face is isolated
             continue
-        
+
         # Get the other face using the optimal edge
         otherFace = optimalEdge.link_faces[0]
         if face == otherFace:
             otherFace = optimalEdge.link_faces[1]
 
-        if mergeList[otherFace] is not None: # Optimal link of the current face is already linked
+        if (
+            mergeList[otherFace] is not None
+        ):  # Optimal link of the current face is already linked
             continue
 
         face_score = squarenessList[optimalEdge]
         otherFace_score = squarenessList[optimalEdges[otherFace]]
-        if face_score > otherFace_score: # Optimal link of the current face has a better link (= smaller score)
+        if (
+            face_score > otherFace_score
+        ):  # Optimal link of the current face has a better link (= smaller score)
             continue
 
         mergeList[face] = otherFace
         mergeList[otherFace] = face
-    
+
     # -- Pure quad-mesh: theoric edge flip and collapse (with some effective changes) --
-    
+
     while True:
         # > Stop condition of the iteration
         existingTriangle = False
@@ -341,16 +357,16 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
 
         i = 0
         l_path = len(path)
-        while i+3 < l_path:
+        while i + 3 < l_path:
             # From (i) (i+1 i+2) (i+3) to (i i+1) (i+2) (i+3)
             # From  A    B - C     D   to  A - B    C     D
-            
-            _,_,subfaceA_BC = commonEdgeOf([path[i]], [path[i+1], path[i+2]])
-            _,_,subfaceBC_D = commonEdgeOf([path[i+3]], [path[i+1], path[i+2]])
+
+            _, _, subfaceA_BC = commonEdgeOf([path[i]], [path[i + 1], path[i + 2]])
+            _, _, subfaceBC_D = commonEdgeOf([path[i + 3]], [path[i + 1], path[i + 2]])
 
             # For a same path A-BC-D there are two possibilities:
-            
-            # Case 0 -> subfaceA_BC != subfaceBC_D 
+
+            # Case 0 -> subfaceA_BC != subfaceBC_D
             #        -> A connected to B, D connected to C
             #        => Simple theoric edge flip between IJ and JK
             # x-------J-------x           x-------x-------x
@@ -360,7 +376,7 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
             #     I-------K-------x           x-------x-------x
 
             if subfaceA_BC == subfaceBC_D:
-                # Case 1 -> subfaceA_BC == subfaceBC_D 
+                # Case 1 -> subfaceA_BC == subfaceBC_D
                 #        -> A connected to B, D not connected to C
                 #        => effective modification of triangles B and C
                 #        ==> now, subfaceA_BC != subfaceBC_D (=> simple theoric edge flip between IJ and JK)
@@ -373,14 +389,14 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
                 #       \   /                       \ . /                       \ | /
                 #        \ /                         \./                         \|/
                 #         0                           K                           K
-                
+
                 # Get the other subface
-                subfaceBC_D = path[i+1]
+                subfaceBC_D = path[i + 1]
                 if subfaceA_BC == subfaceBC_D:
-                    subfaceBC_D = path[i+2]
-                    
+                    subfaceBC_D = path[i + 2]
+
                 # Analyse vertex usage (I,J,K and L are indicated by the schema)
-                commonEdge,verts = merge(subfaceA_BC, subfaceBC_D)
+                commonEdge, verts = merge(subfaceA_BC, subfaceBC_D)
                 for vert in subfaceA_BC.verts:
                     if vert not in commonEdge.verts:
                         vertJ = vert
@@ -389,20 +405,20 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
                     if vert not in commonEdge.verts:
                         vertK = vert
                         break
-                vertI,vertL = commonEdge.verts[0], commonEdge.verts[1]
-                if vertI in path[i+3].verts:
-                    vertI,vertL = vertL,vertI
+                vertI, vertL = commonEdge.verts[0], commonEdge.verts[1]
+                if vertI in path[i + 3].verts:
+                    vertI, vertL = vertL, vertI
 
                 # Tools update
                 squarenessList.pop(commonEdge)
                 mergeList.pop(subfaceA_BC)
                 mergeList.pop(subfaceBC_D)
-                
+
                 # Mesh update
                 mesh.edges.remove(commonEdge)
                 newEdge = mesh.edges.new((vertJ, vertK))
                 mesh.edges.ensure_lookup_table()
-                
+
                 newFaceB = mesh.faces.new([vertI, vertJ, vertK])
                 newFaceC = mesh.faces.new([vertJ, vertK, vertL])
                 mesh.faces.ensure_lookup_table()
@@ -416,15 +432,15 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
             # Theoric edge flip
             mergeList[path[i]] = subfaceA_BC
             mergeList[subfaceA_BC] = path[i]
-            
-            path[i+2] = subfaceBC_D
+
+            path[i + 2] = subfaceBC_D
             i += 2
 
-        if i+1 < l_path:
+        if i + 1 < l_path:
             # From (i) (i+1) to (i i+1)
-            mergeList[path[i]] = path[i+1]
-            mergeList[path[i+1]] = path[i]        
-    
+            mergeList[path[i]] = path[i + 1]
+            mergeList[path[i + 1]] = path[i]
+
     # -- Application: effective edge flip and collapse --
 
     # Tool used to know which face is already merged
@@ -434,15 +450,17 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
     facesToAdd = []
 
     for face in mesh.faces:
-        if merged[face] or mergeList[face] is None: # Face i is already merged or unmergeable
+        if (
+            merged[face] or mergeList[face] is None
+        ):  # Face i is already merged or unmergeable
             continue
-        
+
         otherFace = mergeList[face]
         merged[face] = True
         merged[otherFace] = True
 
         edgeToRemove, faceToAdd = merge(face, otherFace)
-        
+
         edgesToRemove.append(edgeToRemove)
         facesToAdd.append(faceToAdd)
 
@@ -454,6 +472,7 @@ def triToQuad(mesh: bmesh.types.BMesh) -> bmesh.types.BMesh:
         correctQuad(meshFace)
 
     return mesh
+
 
 if __name__ == "__main__":
     # Get the active mesh
@@ -468,5 +487,5 @@ if __name__ == "__main__":
     # Finish up, write the bmesh back to the mesh
     bm.to_mesh(me)
     bm.free()  # free and prevent further access
-    
+
     me.update()
